ebdd7d339acb13402ba5f5541fcc5936ad34d502
alexNeto <alex.neto3@hotmail.com>
2018-03-29 22:53:08 -0300

    desenbabacando um pouco

--- transparencia_api/commons/database_communication.py
+++ transparencia_api/commons/database_communication.py
@@ -2,6 +2,11 @@ import sqlalchemy
 
 
 class DatabaseCommunication:
+    """
+    Classe par criar comunicação com banco de dados
+    Apenas 1 conecção para o projeto
+    """
+
     class __DatabaseCommunication:
         def __init__(self):
             self.user = "transparencia"
@@ -22,11 +27,7 @@ class DatabaseCommunication:
     def __getattr__(self, name):
         return getattr(self.instance, name)
 
-    def connect(self, ):
-        """
-        :rtype: object
-        :return: Returns a connection and a metadata object
-        """
+    def connect(self):
         url = 'postgresql+psycopg2://{}:{}@{}:{}/{}'
         url = url.format(self.user, self.password, self.host, self.port, self.database)
         con = sqlalchemy.create_engine(url)
---
+++ transparencia_api/funcionario_publico/repository/funcionario_publico_repository.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Table, Column, Integer, String, ForeignKey, Sequence
+from transparencia_api.commons.database_communication import DatabaseCommunication
+
+
+class FucionarioPublicoRepository:
+
+    def __init__(self):
+        self.db = DatabaseCommunication().connect()
+        self.funcionario_publico = Table('funcionario_publico', self.db.meta,
+                                         Column('funcionario_publico_id', Integer,
+                                                Sequence('funcionario_publico_id_seq'), primary_key=True),
+                                         Column('cargo_id', Integer, ForeignKey('cargo.cargo_id')),
+                                         Column('dado_salario', Integer, ForeignKey('dado_salario.dado_salario_id')),
+                                         Column('nome', String)
+                                         )
