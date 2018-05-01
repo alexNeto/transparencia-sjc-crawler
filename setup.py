0564ab5ede76c34ca5aad5e02c2c5c1fc096ba26
alexNeto <alex.neto3@hotmail.com>
2018-04-30 22:22:32 -0300

    testando CI e teste de cobertura

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='transparencia-sjc-back',
+    version='0.0.0',
+    url='https://github.com/alexNeto/transparencia-sjc-back',
+    license='MIT License',
+    author='Alexandre Neto',
+    author_email='alex.neto3@hotmail.com',
+    description='Lista todas as remunerações da Câmara Municipal de São José dos Campos',
+    packages=find_packages(),
+    install_requires=[],
+    test_suite='test',
+)
