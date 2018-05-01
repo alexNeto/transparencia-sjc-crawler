378abed1c71d2d1cd17a7aacced3c4dbcd522bec
alexNeto <alex.neto3@hotmail.com>
2018-05-01 11:16:42 -0300

    refatorado insere dos resultados da raspagem de dados

--- transparencia_api/crawler/remuneracao_camara/remuneracao_camara_model.py
+++ transparencia_api/crawler/remuneracao_camara/remuneracao_camara_model.py
@@ -1,21 +1,28 @@
 from transparencia_api.cargo.repository.cargo_repository import CargoRepository
+from transparencia_api.cargo.repository.cargo_storage import CargoStorage
 from transparencia_api.commons.number_utils import to_float
 from transparencia_api.crawler.remuneracao_camara.remuneracao_camara_crawler import RemuneracaoCamaraCrawler
-from transparencia_api.crawler.remuneracao_camara.remuneracao_camara_database_updater import \
-    RemuneracaoCamaraDatabaseUpdater
 from transparencia_api.funcionario_publico.repository.funcionario_publico_repository import FucionarioPublicoRepository
+from transparencia_api.funcionario_publico.repository.funcionario_publico_sotage import FuncionarioPublicoStorage
 from transparencia_api.salario_camara_municipal.repository.salario_camara_municipal_repository import \
     SalarioCamaraMunicipalRepository
+from transparencia_api.salario_camara_municipal.repository.salario_camara_municipal_storage import \
+    SalarioCamaraMunicipalStorage
 
 
 class RemuneracaoCamaraModel:
 
     def __init__(self):
-        self.__crawler_bd = RemuneracaoCamaraDatabaseUpdater()
         self.__data_set = RemuneracaoCamaraCrawler()
+        # Cargo
         self.__cargo = CargoRepository()
+        self.__cargo_storage = CargoStorage()
+        # Salario e data
         self.__salario_camara_municipal = SalarioCamaraMunicipalRepository()
+        self.__salario_camara_municipal_storage = SalarioCamaraMunicipalStorage()
+        # Funcionario
         self.__funcionario_publico = FucionarioPublicoRepository()
+        self.__funcionario_publico_storage = FuncionarioPublicoStorage()
         self.__date = None
         self.__data = None
 
@@ -62,10 +69,11 @@ class RemuneracaoCamaraModel:
         self.set_date()
         for item in self.__data:
             self.set_salario_camara_municipal(item)
-            dado_salario_id = self.__crawler_bd.create_dados_remuneracao(self.__salario_camara_municipal)
+            dado_salario_id = self.__salario_camara_municipal_storage.create_dados_remuneracao(
+                self.__salario_camara_municipal)
             self.set_cargo(item)
-            cargo_id = self.__crawler_bd.create_dados_cargos(self.cargo)
-            date_id = self.__crawler_bd.create_date(self.__date)
-            self.set_funcionario_publico(item, date_id, cargo_id, dado_salario_id)
-            self.__crawler_bd.create_dados_funcionario(self.__funcionario_publico)
+            cargo_id = self.__cargo_storage.create_dados_cargos(self.__cargo)
+            date_id = self.__salario_camara_municipal_storage.create_date(self.__date)
+            self.set_funcionario_publico(item, [date_id, cargo_id, dado_salario_id])
+            self.__funcionario_publico_storage.create_dados_funcionario(self.__funcionario_publico)
         return "done"
